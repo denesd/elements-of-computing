ece6d7f2d71d1a022273e291334f1a83cfd860ea
denesdinok@invenshure.com <denesdinok@invenshure.com>
2021-09-29 14:27:01 +0200

    Add argparse

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -6,25 +6,25 @@ from assembler.assembler import main
 
 def test_max_assembler():
     with tempfile.NamedTemporaryFile(mode="w+") as temp:
-        main(["tests/data/Max.asm", temp.name])
+        main(["tests/data/Max.asm", "--output", temp.name])
         assert filecmp.cmp("tests/data/Max.hack", temp.name, shallow=False)
 
 
 def test_add_assembler():
     with tempfile.NamedTemporaryFile(mode="w+") as temp:
-        main(["tests/data/Add.asm", temp.name])
+        main(["tests/data/Add.asm", "--output", temp.name])
         assert filecmp.cmp("tests/data/Add.hack", temp.name, shallow=False)
 
 
 def test_rect_assembler():
     with tempfile.NamedTemporaryFile(mode="w+") as temp:
-        main(["tests/data/Rect.asm", temp.name])
+        main(["tests/data/Rect.asm", "--output", temp.name])
         assert filecmp.cmp("tests/data/Rect.hack", temp.name, shallow=False)
 
 
 def test_pong_assembler():
     with tempfile.NamedTemporaryFile(mode="w+") as temp:
-        main(["tests/data/Pong.asm", temp.name])
+        main(["tests/data/Pong.asm", "--output", temp.name])
         with open("tests/data/Pong.hack") as f:
             ln = 0
             for l1, l2 in zip(temp, f):
